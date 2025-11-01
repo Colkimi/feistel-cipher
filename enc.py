cd10698e8cf56156dcce607d5e6357162bdb77a1
Collins Mibey <141415279+Colkimi@users.noreply.github.com>
2025-11-01 18:12:42 +0300

    updated file

--- enc.py
+++ enc.py
@@ -2,11 +2,12 @@
 
 from Crypto.Util.number import *
 from gmpy2 import *
+import hashlib
 
-# Secret values
 d = 175
+ROUNDS = 5
 
-get_context().precision = 1337
+get_context().precision = 2048
 
 def pad(m, d):
     if len(str(m)) < d:
@@ -14,19 +15,70 @@ def pad(m, d):
     return int(m)
 
 def genkey(d):
+    p = getPrime(d * 4)
+    q = getPrime(d * 4)
+    n = p * q
+    
     skey = getRandomRange(10 ** (d - 1), 10 ** d)
-    pkey = int(10**d * (sqrt(skey) - floor(sqrt(skey))))
-    return pkey, skey
+    
+    pkey1 = int(10**d * (sqrt(skey) - floor(sqrt(skey))))
+    pkey2 = int(10**d * (cbrt(skey) - floor(cbrt(skey))))
+    pkey3 = int(10**d * (sqrt(skey * 2) - floor(sqrt(skey * 2))))
+    
+    h = int(hashlib.sha512(str(skey).encode()).hexdigest(), 16)
+    pkey4 = h % (10 ** d)
+    
+    return (pkey1, pkey2, pkey3, pkey4, n), (skey, p, q)
 
-def encrypt(m, pkey):
-    m = pad(m, len(str(pkey)))
-    d = len(str(pkey))
-    c = (pkey + d ** 2 * m) % (10 ** d)
+def feistel_round(left, right, round_key, modulus):
+    """Non-linear Feistel function"""
+    temp = (right + round_key) % modulus
+    f_output = pow(temp, 3, modulus)
+    new_left = right
+    new_right = (left + f_output) % modulus
+    return new_left, new_right
+
+def derive_round_keys(pkeys, rounds, m_len):
+    """Derive round keys from public keys"""
+    round_keys = []
+    pkey1, pkey2, pkey3, pkey4, n = pkeys
+    
+    for i in range(rounds):
+        h = hashlib.sha512(f"{pkey1}{pkey2}{pkey3}{pkey4}{i}{m_len}".encode()).digest()
+        rk = int.from_bytes(h, 'big') % n
+        round_keys.append(rk)
+    
+    return round_keys
+
+def encrypt(m, pkeys):
+    pkey1, pkey2, pkey3, pkey4, n = pkeys
+    d = len(str(pkey1))
+    
+    m = pad(m, d)
+    
+    m = (m * pkey1 + pkey2) % n
+    m = pow(m, pkey3 % 65537 | 1, n)  
+    
+    bit_len = m.bit_length()
+    half = bit_len // 2
+    mask = (1 << half) - 1
+    left = m >> half
+    right = m & mask
+    
+    round_keys = derive_round_keys(pkeys, ROUNDS, d)
+    
+    for i in range(ROUNDS):
+        left, right = feistel_round(left, right, round_keys[i], n)
+    
+    c = (left << half) | right
+    
+    c = (c * pkey4 + pkey3 * pkey2) % n
+    c = (c + pkey1 * pkey4) % n
+    
+    c = pow(c, (pkey2 % 256) | 1, n)  
+    
     return c
 
-pkey, skey = genkey(d)
-m = bytes_to_long(flag.encode())
-c = encrypt(m, pkey)
+if __name__ == "__main__":
 
-print(f'pkey = {pkey}')
-print(f'enc  = {c}')
+    pass
